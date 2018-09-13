3024d4b6b324f6ca1b7b7907e9b13afb907e524d
dianatiburcio <42977126+dianatiburcio@users.noreply.github.com>
2018-09-13 16:11:46 -0400

    Add files via upload

---
+++ VariableHW/src/Variables.java
@@ -0,0 +1,25 @@
+
+public class Variables {
+
+	public static void main(String[] args)
+	{
+		String name = "Diana";
+		System.out.print("My name is ");
+		System.out.println(name);
+		
+		int length = 5;
+		int base = 7;
+		int areaR = length * base;
+		System.out.println(areaR);
+		
+		int radius = 3;
+		int diameter = radius * 2;
+		double pi = 3.14;
+		double areaC = radius * radius * pi; 
+		double circC = diameter * pi; 
+		System.out.println(circC);
+		System.out.println(areaC);
+
+	}
+
+}
